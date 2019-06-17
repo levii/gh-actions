15aeb34a98045c04ed9c03205766cdd793a967dc
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-06-18 00:17:22 +0900

    [from now] 2019/06/18 00:17:22
    diff --git a/hello/Dockerfile b/hello/Dockerfile
    index a6abbf4..cf05588 100644
    --- a/hello/Dockerfile
    +++ b/hello/Dockerfile
    @@ -14,4 +14,5 @@ RUN apt-get update \
      && rm -rf /var/lib/apt/lists/*
    
     ADD entrypoint.sh /entrypoint.sh
    +ADD github-event-example /tmp/github-event-example/
     ENTRYPOINT ["/entrypoint.sh"]
    diff --git a/hello/entrypoint.sh b/hello/entrypoint.sh
    index 9f41552..6e06a7b 100755
    --- a/hello/entrypoint.sh
    +++ b/hello/entrypoint.sh
    @@ -1,9 +1,15 @@
     #!/bin/sh -l
    
     set -ex
    -#export GITHUB_EVENT_PATH=/tmp/hello
    -#echo '{"hello": "world"}' > $GITHUB_EVENT_PATH
    +
    +if [ -z "$GITHUB_EVENT_PATH" ]
    +then
    +  export GITHUB_EVENT_PATH=/tmp/github-event-example/pull_request.synchronize.json
    +fi
    
     env
     [ -e "$GITHUB_EVENT_PATH" ] && cat $GITHUB_EVENT_PATH | jq -M .
    -sh -c "echo $*"
    +
    +echo $GITHUB_EVENT_NAME
    +pull_request=`cat $GITHUB_EVENT_PATH | jq -M '.pull_request'`
    +pull_request_base_branch=`echo $pull_request | jq -r '.base.ref'`

--- hello/entrypoint.sh
+++ hello/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -l
 
 set -ex
-#export GITHUB_EVENT_PATH=/tmp/hello
-#echo '{"hello": "world"}' > $GITHUB_EVENT_PATH
+
+if [ -z "$GITHUB_EVENT_PATH" ]
+then
+  export GITHUB_EVENT_PATH=/tmp/github-event-example/pull_request.synchronize.json
+fi
 
 env
 [ -e "$GITHUB_EVENT_PATH" ] && cat $GITHUB_EVENT_PATH | jq -M .
-sh -c "echo $*"
+
+echo $GITHUB_EVENT_NAME
+pull_request=`cat $GITHUB_EVENT_PATH | jq -M '.pull_request'`
+pull_request_base_branch=`echo $pull_request | jq -r '.base.ref'`
